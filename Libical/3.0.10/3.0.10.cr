939ca9863e7dc93ffd62805dcec6b749854d7756
Fulgurance <zohran.londais@gmail.com>
2023-04-18 23:14:04 +0100

    Added Libical

---
+++ Libical/3.0.10/3.0.10.cr
@@ -0,0 +1,38 @@
+class Target < ISM::Software
+
+    def prepare
+        @buildDirectory = true
+        super
+    end
+    
+    def configure
+        super
+
+        runCmakeCommand([   "-DCMAKE_INSTALL_PREFIX=/opt/kf5",
+                            "-DCMAKE_PREFIX_PATH=/opt/qt5",
+                            "-DCMAKE_BUILD_TYPE=Release",
+                            "-DBUILD_TESTING=OFF",
+                            "-Wno-dev",
+                            ".."],
+                            buildDirectoryPath)
+    end
+    
+    def build
+        super
+
+        makeSource(path: buildDirectoryPath)
+    end
+    
+    def prepareInstallation
+        super
+
+        makeSource(["DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+    end
+
+    def install
+        super
+
+        runLdConfigCommand
+    end
+
+end
