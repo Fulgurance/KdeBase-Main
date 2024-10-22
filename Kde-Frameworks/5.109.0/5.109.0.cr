f0046381fd06f3e7d64f4bb79082794190545176
Fulgurance <zohran.londais@gmail.com>
2024-10-22 22:42:33 +0100

    Updated installation process

--- Kde-Frameworks/5.109.0/5.109.0.cr
+++ Kde-Frameworks/5.109.0/5.109.0.cr
@@ -5,17 +5,17 @@ class Target < ISM::SemiVirtualSoftware
 
         makeDirectory("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}etc/profile.d")
 
-        if File.exists?("#{Ism.settings.rootPath}etc/profile.d/kde-framework.sh")
-            copyFile(   "/etc/profile.d/kde-framework.sh",
-                        "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}etc/profile.d/kde-framework.sh")
+        if File.exists?("#{Ism.settings.rootPath}etc/profile.d/kde-frameworks.sh")
+            copyFile(   "/etc/profile.d/kde-frameworks.sh",
+                        "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}etc/profile.d/kde-frameworks.sh")
         else
-            generateEmptyFile("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}etc/profile.d/kde-framework.sh")
+            generateEmptyFile("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}etc/profile.d/kde-frameworks.sh")
         end
 
         kdeFrameworkData = <<-CODE
         export KF5_PREFIX=/usr
         CODE
-        fileUpdateContent("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}etc/profile.d/kde-framework.sh",kdeFrameworkData)
+        fileUpdateContent("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}etc/profile.d/kde-frameworks.sh",kdeFrameworkData)
     end
 
 end
