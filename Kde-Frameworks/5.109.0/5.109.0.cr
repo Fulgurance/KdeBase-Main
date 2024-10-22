7ff75d357775c8a4fe0da97178e11d62692232b2
Fulgurance <zohran.londais@gmail.com>
2024-10-22 22:40:05 +0100

    Updated installation process

--- Kde-Frameworks/5.109.0/5.109.0.cr
+++ Kde-Frameworks/5.109.0/5.109.0.cr
@@ -1,2 +1,21 @@
-class Target < ISM::VirtualSoftware
+class Target < ISM::SemiVirtualSoftware
+
+    def prepare
+        super
+
+        makeDirectory("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}etc/profile.d")
+
+        if File.exists?("#{Ism.settings.rootPath}etc/profile.d/kde-framework.sh")
+            copyFile(   "/etc/profile.d/kde-framework.sh",
+                        "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}etc/profile.d/kde-framework.sh")
+        else
+            generateEmptyFile("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}etc/profile.d/kde-framework.sh")
+        end
+
+        kdeFrameworkData = <<-CODE
+        export KF5_PREFIX=/usr
+        CODE
+        fileUpdateContent("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}etc/profile.d/kde-framework.sh",kdeFrameworkData)
+    end
+
 end
