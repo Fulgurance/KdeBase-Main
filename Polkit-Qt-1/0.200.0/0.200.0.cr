19367a742cef29b1ca8d8a66c92a6ad862d6de96
Fulgurance <zohran.londais@gmail.com>
2025-01-02 15:59:17 +0000

    Added support for qt5

--- Polkit-Qt-1/0.200.0/0.200.0.cr
+++ Polkit-Qt-1/0.200.0/0.200.0.cr
@@ -2,11 +2,25 @@ class Target < ISM::Software
 
     def prepare
         @buildDirectory = true
+        @buildDirectoryNames["MainBuild5"] = "mainBuild5"
         super
     end
 
     def configure
         super
+
+        if option("Qt5")
+            runCmakeCommand(arguments:  "-DCMAKE_INSTALL_PREFIX=/usr        \
+                                        -DCMAKE_INSTALL_LIBEXECDIR=libexec  \
+                                        -DCMAKE_PREFIX_PATH=/usr            \
+                                        -DCMAKE_SKIP_INSTALL_RPATH=ON       \
+                                        -DCMAKE_BUILD_TYPE=Release          \
+                                        -DBUILD_TESTING=OFF                 \
+                                        -DQT_MAJOR_VERSION=5                \
+                                        -Wno-dev                            \
+                                        ..",
+                            path:       buildDirectoryPath("MainBuild5"))
+        end
 
         runCmakeCommand(arguments:  "-DCMAKE_INSTALL_PREFIX=/usr        \
                                     -DCMAKE_INSTALL_LIBEXECDIR=libexec  \
@@ -23,11 +37,20 @@ class Target < ISM::Software
     def build
         super
 
+        if option("Qt5")
+            makeSource(path: buildDirectoryPath("MainBuild5"))
+        end
+
         makeSource(path: buildDirectoryPath)
     end
 
     def prepareInstallation
         super
+
+        if option("Qt5")
+            makeSource( arguments:  "DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath} install",
+                    path:       buildDirectoryPath("MainBuild5"))
+        end
 
         makeSource( arguments:  "DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath} install",
                     path:       buildDirectoryPath)
