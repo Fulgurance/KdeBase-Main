7456187da61b9beeda91d571b6988572687292d1
Fulgurance <zohran.londais@gmail.com>
2025-11-21 11:26:24 +0000

    Added updates and removed unused versions

---
+++ Breeze-Icons/6.20.0/Installer.cr
@@ -0,0 +1,38 @@
+class Target < ISM::Software
+
+    def prepare
+        @buildDirectory = true
+        super
+    end
+
+    def configure
+        super
+
+        runCmakeCommand(arguments:  "-DCMAKE_INSTALL_PREFIX=/usr            \
+                                    -DCMAKE_INSTALL_LIBEXECDIR=libexec      \
+                                    -DCMAKE_PREFIX_PATH=/usr                \
+                                    -DCMAKE_SKIP_INSTALL_RPATH=ON           \
+                                    -DCMAKE_BUILD_TYPE=Release              \
+                                    -DBUILD_TESTING=OFF                     \
+                                    -DPython_EXECUTABLE=/usr/bin/python3.12 \
+                                    -DBINARY_ICONS_RESOURCE=ON              \
+                                    -DSKIP_INSTALL_ICONS=OFF                \
+                                    -Wno-dev                                \
+                                    ..",
+                        path:       buildDirectoryPath)
+    end
+
+    def build
+        super
+
+        makeSource(path: buildDirectoryPath)
+    end
+
+    def prepareInstallation
+        super
+
+        makeSource( arguments:  "DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath} install",
+                    path:       buildDirectoryPath)
+    end
+
+end
---
+++ Sonnet/6.20.0/Installer.cr
@@ -0,0 +1,36 @@
+class Target < ISM::Software
+
+    def prepare
+        @buildDirectory = true
+        super
+    end
+
+    def configure
+        super
+
+        runCmakeCommand(arguments:  "-DCMAKE_INSTALL_PREFIX=/usr        \
+                                    -DCMAKE_INSTALL_LIBEXECDIR=libexec  \
+                                    -DCMAKE_PREFIX_PATH=/usr            \
+                                    -DCMAKE_SKIP_INSTALL_RPATH=ON       \
+                                    -DCMAKE_BUILD_TYPE=Release          \
+                                    -DBUILD_TESTING=OFF                 \
+                                    -DSONNET_NO_BACKENDS=ON             \
+                                    -Wno-dev                            \
+                                    ..",
+                        path:       buildDirectoryPath)
+    end
+
+    def build
+        super
+
+        makeSource(path: buildDirectoryPath)
+    end
+
+    def prepareInstallation
+        super
+
+        makeSource( arguments:  "DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath} install",
+                    path:       buildDirectoryPath)
+    end
+
+end
